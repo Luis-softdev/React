a5e88ad54965d5c6dd85a92ffaff3dd7ead4603f
luisfernand0dev <luisferando.honorato@gmail.com>
2023-04-14 16:41:19 -0300

    Atualizando

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Brain from "./assets/brain.jpg";
+import ManageData from './components/ManageData';
 
 function App() {
   return (
     <div className="App">
       <h2>Avançando em React</h2>
+
+      {/*Imagem em Public*/}
+      <img src="coruja.jpg" alt="Descrever a imagem é uma boa prática"/>
+
+      {/*Imagem em asset*/}
+      <img src={Brain} alt="Cérebro"></img>
+
+      <ManageData/>
     </div>
   );
 }

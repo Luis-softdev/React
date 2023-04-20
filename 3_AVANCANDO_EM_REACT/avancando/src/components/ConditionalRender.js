448ab96be8bc58c25726d2b99e5e1312ecb9dcd6
luisfernand0dev <luisferando.honorato@gmail.com>
2023-04-20 16:47:59 -0300

    Atualizando

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
@@ -1,12 +1,24 @@
 import { useState } from "react"
 
 const ConditionalRender = () => {
-    const [x] = useState(false)
+    const [x] = useState(false);
+    const [name, setName] = useState("Fernando")
   return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
         {/* {!x && <p>Agora X é falso</p>} */}
+        <h1>If Ternário</h1>
+        {name === "Luis" ? (
+          <div>
+            <p>O nome é Luis</p>
+          </div>
+        ) : (
+          <div>
+            <p>Nome não econtrado</p>
+          </div>
+        )}
+        <button onClick={() => setName("Luis")}>Clique aqui</button>
     </div>
   );
 };

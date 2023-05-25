400f5031320f224b9ee4d985e148be632944776f
luisfernand0dev <luisferando.honorato@gmail.com>
2023-05-25 16:53:42 -0300

    Atualizando

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useState } from 'react';
 import './App.css';
 import Brain from "./assets/brain.jpg";
 import ConditionalRender from './components/ConditionalRender';
@@ -6,11 +6,18 @@ import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
 import ShowUserName from './components/ShowUserName';
 import CarDatails from './components/CarDatails';
+import Fragment from './components/Fragment';
 
 function App() {
 
-  const name = "Fernando"
+  //const name = "Fernando"
   const [userName] = useState("Luis Fernando")
+
+  const cars = [
+    {id: 1, brand: "Ferrari", color: "Vermelha", Km: 0, newCar: true},
+    {id: 2, brand: "VW", color: "Prata", Km: 4500, newCar: false},
+    {id: 3, brand: "Hianday", color: "Branco", Km: 0, newCar: true}
+  ]
 
   return (
     <div className="App">
@@ -28,7 +35,18 @@ function App() {
       {/* props */}
       <ShowUserName name={userName}/>
       {/* destructuring */}
-      <CarDatails brand="VW" km={100000} color="Azul" />
+      <CarDatails brand="VW" km={100000} color="Azul" newCar={false}/>
+      {/* reaproveitando*/}
+      <CarDatails brand="Ford" km={0} color="Vermelho" newCar={true}/>
+      <CarDatails brand="Fiat" km={5400} color="Preto" newCar={false}/>
+      {/* loop em array de objetos*/}
+      {cars.map((car) => (
+        <CarDatails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar}/>))}
+      {/*fragment*/}
+      <Fragment propFragment="teste" />
+
+
+      
     </div>
   );
 }
--- 3_AVANCANDO_EM_REACT/avancando/src/components/CarDatails.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/CarDatails.js
@@ -1,12 +1,13 @@
-const CarDatails = ({brand, km, color}) => {
+const CarDatails = ({brand, km, color, newCar}) => {
   return (
     <div>
         <h2>Detalhes do Carro</h2>
         <ul>
             <li>Marca: {brand}</li>
             <li>KM: {km}</li>
-            <li>Cor: {clscolor}</li>
+            <li>Cor: {color}</li>
         </ul>
+        {newCar && <p>Esse Carro é novo</p>}
     </div>
   )
 }

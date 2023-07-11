2b6487f195dada72841b8a3550dc33cfd6e30f2a
luisfernand0dev <luisferando.honorato@gmail.com>
2023-07-11 18:44:53 -0300

    Atualizando

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -1,9 +1,45 @@
+//CSS
 import './App.css';
 
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+//data
+import {wordsList} from "./data/words"
+
+//components
+import StartScreen from './data/components/StartScreen';
+import Game from './data/components/Game';
+import GameOver from './data/components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList);
+  
+  //start the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+  //restart the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
 }

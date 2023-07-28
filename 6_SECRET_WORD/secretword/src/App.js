d91e0e7724277ba9e89b7873a0fc333e8238647c
luisfernand0dev <luisferando.honorato@gmail.com>
2023-07-28 18:03:57 -0300

    Atualizando , porém tem alguma coisa errada

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -26,6 +26,12 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("")
   const [letters, setLetters] = useState([])
 
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLettters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setCore] = useState(0)
+
+
   const pickWordAndCategory = () => {
     // pick a random category
     const categories = Object.keys(words)
@@ -50,6 +56,7 @@ function App() {
     
     //create an array of letters
     let wordLetters = word.split("")
+    
     wordLetters = wordLetters.map((l) => l.toLowerCase())
 
     console.log(word, category);
@@ -58,7 +65,7 @@ function App() {
     //fill states
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name)
   }
@@ -74,7 +81,16 @@ function App() {
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'game' && <Game 
+      verifyLetter={verifyLetter}
+      pickedWord={pickedWord}
+      pickedCategory={pickedCategory}
+      letters={letters}
+      guessedLetters={guessedLetters}
+      wrongLettters={wrongLettters}
+      guesses={guesses}
+      score={score}
+      />}
       {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
